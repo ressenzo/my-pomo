e8635463935a25294115e9579b531fef00416173
Renan <renancossenzo@hotmail.com>
2025-04-23 12:48:29 -0300

    Fix in action and intervals logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,50 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 const App = () => {
-	const SMALL_INTERVAL_TIME: number = 2;
-	const SMALL_INTERVAL_QUANTITY: number = 3;
-	const LONG_INTERVAL_TIME: number = 4;
-	const LONG_INTERVAL_QUANTITY: number = 1;
-
 	const IN_ACTION_TIME: number = 5;
+	const SMALL_INTERVAL_TIME: number = 2;
+	const LONG_INTERVAL_TIME: number = 4;
 	
-	const FULL_CYCLE_INTERVAL_QUANTITY: number = SMALL_INTERVAL_QUANTITY + LONG_INTERVAL_QUANTITY;
-
 	const [inActionQuantity, setInActionQuantity] = useState<number>(0);
-	const [intervalQuantity, setIntervalQuantity] = useState<number>(0);
+	const [smallIntervalQuantity, setSmallIntervalQuantity] = useState<number>(0);
+	const [longIntervalQuantity, setLongIntervalQuantity] = useState<number>(0);
 	const [time, setTime] = useState<number>(0);
 	const [isInterval, setIsInterval] = useState<boolean>(false);
 
 	useEffect(() => {
-		
-		if (inActionQuantity > 0) {
-			if (time === 0) return;
+		if (time === 0) return;
 
-			const interval = setInterval(() => {
-				setTime(p => p -1);
-			}, 1000)
-			
-			return () => clearInterval(interval);
-		}
+		const interval = setInterval(() => {
+			setTime(v => v - 1);
+		}, 1000)
+		
+		return () => clearInterval(interval);
 	}, [time])
 
 	const startTime = () => {
-		if (intervalQuantity == 0 || !isInterval)
-		{
-			setIsInterval(true);
-			setInActionQuantity(qtt => qtt + 1);
-			setTime(IN_ACTION_TIME);
-			return;
-		}
-
-		setIsInterval(false);
-		if (inActionQuantity % 4 === 0)
+		if (isInterval && inActionQuantity % 4 === 0) {
 			setTime(LONG_INTERVAL_TIME);
-		else
+			setIsInterval(false);
+			setLongIntervalQuantity(v => v + 1);
+		} else if (isInterval) {
 			setTime(SMALL_INTERVAL_TIME);
-		// if (intervalQuantity % 4 === 0) {
-		// 	setTime(SMALL_INTERVAL_TIME);
-		// }
+			setIsInterval(false);
+			setSmallIntervalQuantity(v => v + 1);
+		} else {
+			setInActionQuantity(v => v + 1);
+			setTime(IN_ACTION_TIME);
+			setIsInterval(true);
+		}
 	}
 
 	return (
 		<div className="App">
 			<h1>Hello</h1>
 			<h1>{time}</h1>
-			<h1>Small intervals: {intervalQuantity - Math.floor(intervalQuantity / FULL_CYCLE_INTERVAL_QUANTITY)}</h1>
-			<h1>Long intervals: {Math.floor(intervalQuantity / FULL_CYCLE_INTERVAL_QUANTITY)}</h1>
-			<h1>Intervals: {intervalQuantity}</h1>
+			<h1>In action quantity: {inActionQuantity}</h1>
+			<h1>Small intervals: {smallIntervalQuantity}</h1>
+			<h1>Long intervals: {longIntervalQuantity}</h1>
 			<button onClick={() => startTime()}>Start</button>
 		</div>
 	);
